a83018e59c28f8c8170b0c86fdead5655644d251
Qingyun Liu <qingyunliu@Qingyuns-MacBook-Pro.local>
2018-11-15 22:54:45 +0100

    plot_data.py

--- plot_data.py
+++ plot_data.py
@@ -1,39 +1,36 @@
 import matplotlib.pyplot as plt
-# matplotlib.pyplot is a collection of command style functions to make matplotlib work like MATLAB. 
-# each pyplot function makes changes to the figure, like decorates the plot with labels, 
-# plot some lines in the plotting area.
 import math
 import numpy as np
-#np.set_printoptions(threshold=np.nan) # to show the whole array
-# numpy is the core library for scientific computing in Python. It provides high-perfornmance multidimonesial 
-# array bojects, and tools for working with these arrays. 
-# plt.close("all")
 import pandas as pd
-# pandas stands for python data analysis library, it is a library providing high-performance,
-# easy-to-use data structures and data analysis tools for the Python programming language.
-# it takes data(like CSV or TSV) and creates a python project with rows and columns called data frame that looks very 
-# similar to table in statistical software.
-def normalization(n,thefile): 
+
+def normalization(k,n,thefile): 
     """Normalization of the curve"""
+    mm = np.zeros((k,n))
+    x = thefile[:,0]
+    mm[:,0] = x
     for i in range(1,n):
-        y = thefile.values[:,i]
+        #y = thefile.iloc[:,i]
+        y = thefile[:,i]
         Nrml_y = y/max(y)
-        plt.plot(x,Nrml_y)
-        print(Nrml_y)
+        [k,l]= thefile.shape
+        mm[:,i] = Nrml_y
+    return mm
+    print(k,l,mm)
+    #plt.plot(x,mm[:,1:]) 
+    #plt.show()
         #plt.hold(True) 
-    return i
+    
     #plt.xlim(380,700)
     #plt.xlabel("Wavelength")
     #plt.ylabel("Intensity")
-    #plt.show()
-file = pd.read_excel("w_wout_lens_0.57A_980.xlsx")
-print(file)
-file.as_matrix()
-[k,l] = file.shape
-print(k)
-print(l)
-print(file)
-x = file.values[:,0]
-if __name__ == "__main__": 
-    normalization(l,file)
     
+file = pd.read_excel("w_wout_lens_0.57A_980.xlsx")
+file = file.as_matrix()
+print(file)
+[k,l] = file.shape
+print(k,l)
+x = file[:,0]
+xa = x.shape
+print(x,xa)
+if __name__ == "__main__": 
+    normalization(k,l,file)
